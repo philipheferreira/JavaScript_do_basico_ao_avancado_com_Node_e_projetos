74de36f16e0fca17d3521eca6bc583b529896999
philipheferreira <philipheferreira@gmail.com>
2024-06-03 16:15:36 -0300

    upgrade: firt project amostra html

---
+++ 17-Eventos com javascript/projects/1 - mensagemHTML/script.js
@@ -0,0 +1,22 @@
+//Variaveis do programa
+let btn1 = document.querySelector(".botao1");
+let btn2 = document.querySelector(".botao2");
+let msg = document.querySelector(".mensagem");
+let mostrar = document.querySelector(".mostrador");
+
+//eventos
+
+btn1.addEventListener("click", Amostra);
+	
+btn2.addEventListener("click", Esconde);
+//funções
+
+function Amostra(){
+	msg.innerHTML = "Agora você pode ver a mensagem secreta";
+	mostrar.innerHTML = "Amora";
+}
+
+function Esconde(){
+	msg.innerHTML = "Agora a mensagem secreta está escondida";
+	mostrar.innerHTML = "";
+}
--- 17-Eventos com javascript/projects/teste/script.js
+++
@@ -1,9 +0,0 @@
-
-let x;
-
-let btn = document.querySelector(".botao");
-
-btn.addEventListener("click", function(){
-	console.log("Você clicou no botão");
-});
-
