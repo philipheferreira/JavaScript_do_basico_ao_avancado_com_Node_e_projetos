57b09ce7fb06c7e32780a31fbf095e65f928b47b
philipheferreira <philipheferreira@gmail.com>
2024-06-04 10:47:34 -0300

    upgrade: 3 project function add and sub

--- 17-Eventos com javascript/projects/3 - counter click button/script.js
+++ 17-Eventos com javascript/projects/3 - counter click button/script.js
@@ -1,2 +1,17 @@
 btn1 = document.querySelector(".botao1");
 btn2 = document.querySelector(".botao2");
+count = 0;
+
+btn1.addEventListener("click", adicionar);
+
+btn2.addEventListener("click", subtrair);
+
+function adicionar(){
+	count++;
+	console.log(count);
+}
+
+function subtrair(){
+	count--;
+	console.log(count);
+}
