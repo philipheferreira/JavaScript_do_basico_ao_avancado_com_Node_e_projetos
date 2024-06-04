df7e313958acbe3ff706a8bcbd870bd85ac4b896
philipheferreira <philipheferreira@gmail.com>
2024-06-04 11:13:57 -0300

    update: count tela innerHTML

--- 17-Eventos com javascript/projects/3 - counter click button/script.js
+++ 17-Eventos com javascript/projects/3 - counter click button/script.js
@@ -1,6 +1,7 @@
-btn1 = document.querySelector(".botao1");
-btn2 = document.querySelector(".botao2");
-count = 0;
+let btn1 = document.querySelector(".botao1");
+let btn2 = document.querySelector(".botao2");
+let tela = document.querySelector(".mostrador");
+let count = 0;
 
 btn1.addEventListener("click", adicionar);
 
@@ -9,9 +10,11 @@ btn2.addEventListener("click", subtrair);
 function adicionar(){
 	count++;
 	console.log(count);
+	tela.innerHTML = count;
 }
 
 function subtrair(){
 	count--;
 	console.log(count);
+	tela.InnerHTML = count
 }
