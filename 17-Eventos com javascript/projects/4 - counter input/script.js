09c2961d19ce45174df116ab12c36ae13dc41a38
philipheferreira <philipheferreira@gmail.com>
2024-06-05 17:06:11 -0300

    update: 4 project receve value

--- 17-Eventos com javascript/projects/4 - counter input/script.js
+++ 17-Eventos com javascript/projects/4 - counter input/script.js
@@ -1,2 +1,10 @@
-btn1 = document.querySelector(".botao1");
+let btn1 = document.querySelector(".botao1");
+let escrita = document.querySelector(".valorVariavel");
+let mostrar = document.querySelector(".mostrador");
+let x = 0;
 
+btn1.addEventListener("click", function(){
+	x = escrita.value;
+	mostrar.innerHTML = mostrar.innerHTML + x;
+
+});
