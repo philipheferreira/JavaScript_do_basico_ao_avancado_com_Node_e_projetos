0fc2abe27903cfbc14db63d599dbb09b48ab16c4
philipheferreira <philipheferreira@gmail.com>
2024-06-06 09:33:14 -0300

    upgrade: correction

--- 17-Eventos com javascript/projects/4 - counter input/script.js
+++ 17-Eventos com javascript/projects/4 - counter input/script.js
@@ -1,10 +1,10 @@
 let btn1 = document.querySelector(".botao1");
 let escrita = document.querySelector(".valorVariavel");
 let mostrar = document.querySelector(".mostrador");
-let x = 0;
-
+let x = 0; // precisar começar com um valor zerado para poder ser contabilizado como number;
+mostrar.innerHTML = 0; // precisa ter um numero inicializado com 0 se não quando for conversert com parseInt na soma ira dar NaN
 btn1.addEventListener("click", function(){
 	x = escrita.value;
-	mostrar.innerHTML = mostrar.innerHTML + x;
+	mostrar.innerHTML = parseInt(mostrar.innerHTML) + parseInt(x); // parseInt faz a conversão de numeros
 
 });
