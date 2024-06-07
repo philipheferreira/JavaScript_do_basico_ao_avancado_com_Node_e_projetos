d8478001a20455ab7ee75d7f38d57814717f016d
philipheferreira <philipheferreira@gmail.com>
2024-06-07 16:55:12 -0300

    upgrade: project 5 script adjust

--- 17-Eventos com javascript/projects/5 - show image/script.js
+++ 17-Eventos com javascript/projects/5 - show image/script.js
@@ -1,3 +1,21 @@
 btn = document.querySelector("botao");
 let valor = 0
 
+btn.addEventListener("click", mudar){
+
+}
+
+function mudar(){
+
+	if(valor == 1){
+
+	}else
+	if(valor == 2){
+
+	}else
+	if(valor == 3){
+
+	}else{
+		
+	}
+}
