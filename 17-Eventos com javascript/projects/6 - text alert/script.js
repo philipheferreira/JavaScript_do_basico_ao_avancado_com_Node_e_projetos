4672aa1a8578f97845b9f38f0bdf7adaddc42a52
philipheferreira <philipheferreira@gmail.com>
2024-06-08 18:36:50 -0300

    Upgrade: text alert function

--- 17-Eventos com javascript/projects/6 - text alert/script.js
+++ 17-Eventos com javascript/projects/6 - text alert/script.js
@@ -1,1 +1,8 @@
-btn = document.querySelector(".botao");
+let btn = document.querySelector(".botao");
+let valorNome = document.querySelector(".nomeInput");
+let nome = "";
+btn.addEventListener("click", function(){
+	nome = valorNome.value;
+	alert("Ola, seu nome é " + nome);
+	valorNome.value = ""; // serve para zerar novamentee o input
+});
