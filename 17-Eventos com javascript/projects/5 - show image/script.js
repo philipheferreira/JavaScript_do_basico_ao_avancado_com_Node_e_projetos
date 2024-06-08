66144943f4ac368eaa8ba3d3d96cdcd54541de52
philipheferreira <philipheferreira@gmail.com>
2024-06-08 17:35:17 -0300

    upgrade: project image progress logic js

--- 17-Eventos com javascript/projects/5 - show image/script.js
+++ 17-Eventos com javascript/projects/5 - show image/script.js
@@ -1,21 +1,24 @@
-btn = document.querySelector("botao");
+btn = document.querySelector(".botao");
+
 let valor = 0
 
-btn.addEventListener("click", mudar){
-
-}
+btn.addEventListener("click", mudar);
 
 function mudar(){
-
+	valor++;
 	if(valor == 1){
-
+		document.querySelector(".imagem").src = "img/naruto.jpg"
+		console.log(valor);
 	}else
 	if(valor == 2){
-
+		document.querySelector(".image").src = "img/dragon_ball.jpg"
+		console.log(valor);
 	}else
 	if(valor == 3){
-
+		document.querySelector(".image").src = "img/one_peace.jpg"
+		console.log(valor);
 	}else{
-		
+		console.log(valor);
 	}
+
 }
