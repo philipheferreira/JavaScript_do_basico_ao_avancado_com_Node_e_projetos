2d688457ab3caedbeb5a5a5a433451ea5440b219
philipheferreira <philipheferreira@gmail.com>
2024-06-10 16:36:17 -0300

    Upgrade: button change backgroundcolor fish logic js

--- 17-Eventos com javascript/projects/7 - change background color button/script.js
+++ 17-Eventos com javascript/projects/7 - change background color button/script.js
@@ -1,5 +1,8 @@
 btn = document.querySelector(".botao");
 
 btn.addEventListener("click", function(){
-	console.log("Click esta funcionando");
+	document.body.style.backgroundColor = 
+	'rgb(' + Math.round(Math.random() * 255) + 
+	',' + Math.round(Math.random() * 255) + 
+	',' + Math.round(Math.random() * 255) + ')';
 })
