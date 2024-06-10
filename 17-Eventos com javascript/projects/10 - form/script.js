93961e08c32b0bbf275280196a6c5e06e398b964
philipheferreira <philipheferreira@gmail.com>
2024-06-09 21:45:57 -0300

    upgradee: form project finish

--- 17-Eventos com javascript/projects/10 - form/script.js
+++ 17-Eventos com javascript/projects/10 - form/script.js
@@ -1,8 +1,13 @@
 let btn = document.querySelector(".enviar");
 let nome = document.querySelector(".nome");
+let telefone = document.querySelector(".telefone");
+let email = document.querySelector(".email");
+
 
 btn.addEventListener("click", function(){
-
-		alert("Ola Sr(a) " + nome.value + ", O formulario foi enviado com sucesso");	
-	
+		console.log("Informações preenchidas");
+		console.log("Nome: " + nome.value);
+		console.log("Telefone:" + telefone.value);
+		console.log("email" + email.value);	
+		alert("Suas informações são as seguintes. Nome:" + nome.value + "\nTelefone" + telefone.value + "\nemail: " + email.value + ".");
 });
