2b48a0cd699ee222dc6601883f48f50db122b20b
philipheferreira <philipheferreira@gmail.com>
2024-06-18 15:38:35 -0300

    Upgrade: project function finish guess number

--- 17-Eventos com javascript/projects/9 - number guessing game/script.js
+++ 17-Eventos com javascript/projects/9 - number guessing game/script.js
@@ -1,10 +1,19 @@
 // gera automaticamente o numero dentro da variavel valor, entre 0 a 10 + 1;
 let valor = Math.floor(Math.random() * 10 + 1);
-let valorEscrito = document.querySelector(".Escrito");
 
+let valorEscrito = document.querySelector(".Escrito");
 let btn = document.querySelector(".botao");
 
 btn.addEventListener("click", function(){
-	x = valorEscrito.value;
-	console.log("Você digitou o numero " + x);
+	let x = valorEscrito.value;
+
+	if(x == valor){
+		alert("PARABÉNS, VOCÊ ACERTO!!!!\nO numero era "+ valor);
+	}else if (x > valor) {
+		alert("Errou, tente um numero menor");
+	}else {
+		alert("Errou, tente um numero maior");
+	}
+
+
 });
