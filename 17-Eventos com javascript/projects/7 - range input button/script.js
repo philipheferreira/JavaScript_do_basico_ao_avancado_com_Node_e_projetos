dc74180ba47bbaa281cd71a9ba82732fb0bb8b28
philipheferreira <philipheferreira@gmail.com>
2024-06-24 11:29:45 -0300

    Upgrade: front css

--- 17-Eventos com javascript/projects/7 - range input button/script.js
+++ 17-Eventos com javascript/projects/7 - range input button/script.js
@@ -2,6 +2,8 @@ let variavel = document.querySelector(".meuRange");
 let btn = document.querySelector(".botao");
 let mostrador = document.querySelector(".visor");
 
+mostrador.innerHTML = 0;
+variavel.value = 0;
 
 btn.addEventListener("click", function(){
 	x = variavel.value;
