fc6b7893965a6728b468d1ea432f1c2292a7a457
pferreira <philiphe.ferreira@w3as.com.br>
2024-06-26 15:48:09 -0300

    Upgrade: clock 1 new things and clock 2 basic template

---
+++ 17-Eventos com javascript/projects/12 - clock 1/script.js
@@ -0,0 +1,6 @@
+function iniciarRelogio(){
+	const hoje = new Date(); /* A constante hoje recebe a new Date nas suas operações */
+	let horas = hoje.getHours(); /*  */
+	let minutos = hoje.getMinutes(); /*  */
+	let segundos = hoje.getSeconds(); /*  */
+}
