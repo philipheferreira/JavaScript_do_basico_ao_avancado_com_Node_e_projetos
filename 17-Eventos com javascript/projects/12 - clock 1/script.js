4ea97a112f3ef29319820248f0212edcca58cad1
pferreira <philiphe.ferreira@w3as.com.br>
2024-06-29 09:51:35 -0300

    update: things i forgot

--- 17-Eventos com javascript/projects/12 - clock 1/script.js
+++ 17-Eventos com javascript/projects/12 - clock 1/script.js
@@ -3,4 +3,7 @@ function iniciarRelogio(){
 	let horas = hoje.getHours(); /*  */
 	let minutos = hoje.getMinutes(); /*  */
 	let segundos = hoje.getSeconds(); /*  */
+	document.querySelector(".relogio").innerHTML = horas + ":" + minutos + ":" + segundos;
+	setTimeout(startTime, 1000); /* A cada 1 segundo a pagina reenvia informações */ 
 }
+
