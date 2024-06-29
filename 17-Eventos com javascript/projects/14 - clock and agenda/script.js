0b3256d3347eaf650f4770816dd16b3322b3807e
ComputadorCozinha <philipheferreira@yahoo.com.br>
2024-06-29 17:48:23 -0300

    upgrade: agenda project time function

---
+++ 17-Eventos com javascript/projects/14 - clock and agenda/script.js
@@ -0,0 +1,18 @@
+let campoHoras = document.querySelector(".horas");
+let campoMinutos = document.querySelector(".minutos");
+let campoSegundos = document.querySelector(".segundos");
+
+let iniciarTempo = () => {
+	let hoje = new Date();
+	let hora = hoje.getHours();
+	let minuto = hoje.getMinutes();
+	let segundo = hoje.getSeconds();
+
+	campoHoras.innerHTML = hora;
+	campoMinutos.innerHTML = minuto;
+	campoSegundos.innerHTML = segundo;
+
+	setTimeout(iniciarTempo, 1000);
+}
+
+document.onload = iniciarTempo()
