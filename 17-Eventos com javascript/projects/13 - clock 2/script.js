07c2b446255f8c8d7c3c3426d0f9edee916ac99b
ComputadorCozinha <philipheferreira@yahoo.com.br>
2024-06-29 11:32:46 -0300

    Upgrade: function update correct

--- 17-Eventos com javascript/projects/13 - clock 2/script.js
+++ 17-Eventos com javascript/projects/13 - clock 2/script.js
@@ -5,12 +5,10 @@ localSegundos = document.querySelector(".segundos")
 
 let iniciarRelogio = () => {
 
-	const hoje = new Date();
+	const hoje = new Date(); // precisa estar na função
 	let hora = hoje.getHours();
 	let minuto = hoje.getMinutes();
 	let segundo = hoje.getSeconds();
-
-
 	localHoras.innerHTML = hora;
 	localMinutos.innerHTML = minuto;
 	localSegundos.innerHTML = segundo;
