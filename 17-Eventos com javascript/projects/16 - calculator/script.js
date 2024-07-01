a1745ab3a9558954b9420dc54e705048712c24a8
philipheferreira <philipheferreira@gmail.com>
2024-07-01 14:41:00 -0300

    Upgrade: calculator basic functions

--- 17-Eventos com javascript/projects/16 - calculator/script.js
+++ 17-Eventos com javascript/projects/16 - calculator/script.js
@@ -1,4 +1,5 @@
-// Variaveis de botões criadas para botões
+// Variaveis criadas para receber os apontamentos no html
+let mostrador = document.querySelector(".visor")
 let btnZero = document.querySelector(".zero");
 let btnUm = document.querySelector(".um");
 let btnDois = document.querySelector(".dois");
@@ -13,8 +14,72 @@ let btnNove = document.querySelector(".nove");
 //funções criadas para ouvir eventos
 
 let funcaoZero = () => {
+	mostrador.innerHTML = "0";
 	console.log("O valor é zero");
+}
+
+let funcaoUm = () => {
+	mostrador.innerHTML = 1;
+	console.log("O valor é um");
+}
+
+let funcaoDois = () => {
+	mostrador.innerHTML = 2;
+	console.log("O valor é dois");
+}
+
+let funcaoTres = () => { 
+	mostrador.innerHTML = 3;
+	console.log("O valor é três");
+}
+
+let funcaoQuatro = () => {
+	mostrador.innerHTML = 4;
+	console.log("O valor é quatro");
+}
+
+let funcaoCinco = () => {
+	mostrador.innerHTML = 5;
+	console.log("O valor é cinco");
+}
+
+let funcaoSeis = () => {
+	mostrador.innerHTML = 6;
+	console.log("O valor é seis");
+}
+
+let funcaoSete = () => {
+	mostrador.innerHTML = 7;
+	console.log("O valor é sete");
+}
+
+let funcaoOito = () => {
+	mostrador.innerHTML = 8;
+	console.log("O valor é oito");
+}
+
+let funcaoNove = () => {
+	mostrador.innerHTML = 9;
+	console.log("O valor é nove");
 }
 
 // criação de escutadores de evento
 btnZero.addEventListener("click", funcaoZero);
+
+btnUm.addEventListener("click", funcaoUm);
+
+btnDois.addEventListener("click", funcaoDois);
+
+btnTres.addEventListener("click", funcaoTres);
+
+btnQuatro.addEventListener("click", funcaoQuatro);
+
+btnCinco.addEventListener("click", funcaoCinco);
+
+btnSeis.addEventListener("click", funcaoSeis);
+
+btnSete.addEventListener("click", funcaoSete);
+
+btnOito.addEventListener("click", funcaoOito);
+
+btnNove.addEventListener("click", funcaoNove);
