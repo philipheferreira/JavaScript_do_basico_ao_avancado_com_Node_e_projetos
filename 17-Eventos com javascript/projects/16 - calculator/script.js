36d8d40b6739dcc34b11da8a055875af8758bbfb
philipheferreira <philipheferreira@gmail.com>
2024-07-01 14:19:29 -0300

    Upgrade: calculator buttons query

---
+++ 17-Eventos com javascript/projects/16 - calculator/script.js
@@ -0,0 +1,12 @@
+// Variaveis de botões criadas para botões
+let btnZero = document.querySelector(".zero");
+let btnUm = document.querySelector(".um");
+let btnDois = document.querySelector(".dois");
+let btnTres = document.querySelector(".tres");
+let btnQuatro = document.querySelector(".quatro");
+let btnCinco = document.querySelector(".cinco");
+let btnSeis = document.querySelector(".seis");
+let btnSete = document.querySelector(".sete");
+let btnOito = document.querySelector(".oito");
+let btnNove = document.querySelector(".nove");
+
