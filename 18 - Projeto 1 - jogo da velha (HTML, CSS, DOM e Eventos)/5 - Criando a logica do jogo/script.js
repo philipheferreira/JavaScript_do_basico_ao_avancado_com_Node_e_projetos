fa3ac65fa11885e0bfa36a523ca43bffa5b6d879
philipheferreira <philipheferreira@gmail.com>
2024-08-02 16:47:25 -0300

    Feat: first project cap 5 create querys

--- 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/5 - Criando a logica do jogo/script.js
+++ 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/5 - Criando a logica do jogo/script.js
@@ -1,5 +1,12 @@
-let btn = document.querySelector(".treino");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let botoes = document.querySelectorAll("");
+let mensagemContainer = document.querySelector(".mensagem p");
+let segundoJogador;
 
-btn.addEventListener("click", function(){
-	console.log("Click");
-});
+// Contador de jogadas
+let jogador1 = 0;
+let jogador2 = 0;
+
+// adicionando o evento de click aos boxes
