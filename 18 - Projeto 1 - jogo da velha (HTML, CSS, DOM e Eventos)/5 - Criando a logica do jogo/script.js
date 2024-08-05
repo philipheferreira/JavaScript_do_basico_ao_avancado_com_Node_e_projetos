3dabe299ca9f7f7322d9c5f595ac34eadd47ecd2
pferreira <philiphe.ferreira@w3as.com.br>
2024-08-05 15:02:15 -0300

    Feat: Error X box correction

--- 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/5 - Criando a logica do jogo/script.js
+++ 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/5 - Criando a logica do jogo/script.js
@@ -1,8 +1,9 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let botoes = document.querySelectorAll("");
-let mensagemContainer = document.querySelector(".mensagem p");
+let botoes = document.querySelectorAll(".container-botao button");
+//let messagemContainer = document.querySelector("")
+let mensagemTexto = document.querySelector(".mensagem p");
 let segundoJogador;
 
 // Contador de jogadas
@@ -10,3 +11,22 @@ let jogador1 = 0;
 let jogador2 = 0;
 
 // adicionando o evento de click aos boxes
+
+for(let i = 0; i < boxes.length; i++){
+
+	boxes[i].addEventListener("click", function(){
+
+		let el;
+
+		if(jogador1 == jogador2){
+			el = x;
+		} else {
+			el = o;
+		}
+
+		let cloneEl = el.cloneNode(true);
+
+		this.appendChild(cloneEl);
+	});
+
+}
