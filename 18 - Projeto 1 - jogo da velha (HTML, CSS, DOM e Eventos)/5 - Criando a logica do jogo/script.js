922248a578c736bff724c4253df4771d89ed9327
philipheferreira <philipheferreira@gmail.com>
2024-08-07 14:55:20 -0300

    Feat: project 1 correction turns players game

--- 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/5 - Criando a logica do jogo/script.js
+++ 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/5 - Criando a logica do jogo/script.js
@@ -19,14 +19,26 @@ for(let i = 0; i < boxes.length; i++){
 		let el; // variavel criada para receber e repassar a situação de x ou 0
 
 		if(jogador1 == jogador2){ // verifica a situação do valor dentro das variaveis jogadores e se forem iguais realiza a ação
-			el = x;
+			// 
+			el = x; // ira colocar dentro da variavel o valor x que representa o elemento x dentro do css
 		} else {
-			el = o;
+			el = o; // ira colocar o valor de circulo dentro da variavel que foi criada no html e css
+			//
 		}
 
-		let cloneEl = el.cloneNode(true);
+		// logica para computar cada uma das jogadas de cada um
+		if(jogador1 == jogador2){ // quando o valor dos jogadores estiver igual ele ira somar +1 para o jogador 1
+			jogador1++;
+		}else{ // como a unica outra situação é um valor diferente entre as variaveis ira aumentar o valor do jogador 2 em +1
+			jogador2++;
+		}
 
-		this.appendChild(cloneEl);
+		let cloneEl = el.cloneNode(true); // cria uma variavel para clonar dentro dela todo o elemento el
+
+		this.appendChild(cloneEl); // adiciona o clone do elemento dentro da caixa
+
+
+
 
 	});
 
