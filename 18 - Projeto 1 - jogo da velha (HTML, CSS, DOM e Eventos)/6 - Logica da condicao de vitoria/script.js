4d24659ce56061c1bcb9ac75396f5859a43accda
philipheferreira <philipheferreira@gmail.com>
2024-08-08 14:43:10 -0300

    project 1 6cap template basic

--- 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/6 - Logica da condicao de vitoria/script.js
+++ 18 - Projeto 1 - jogo da velha (HTML, CSS, DOM e Eventos)/6 - Logica da condicao de vitoria/script.js
@@ -1,5 +1,63 @@
-let btn = document.querySelector(".treino");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let botoes = document.querySelectorAll(".container-botao button");
+//let messagemContainer = document.querySelector("")
+let mensagemTexto = document.querySelector(".mensagem p");
+let segundoJogador;
 
-btn.addEventListener("click", function(){
-	console.log("Click");
-});
+// Contador de jogadas
+let jogador1 = 0;
+let jogador2 = 0;
+
+
+
+// adicionando o evento de click aos boxes
+
+for(let i = 0; i < boxes.length; i++){
+
+	boxes[i].addEventListener("click", function(){ // Gera um evento que afeta todos os itens da classe box
+
+	let el = checarElemento(jogador1, jogador2);
+
+	if(this.childNodes.length == 0){ // verifica se o box tem preenchido algum valor, se não tiver nada dentro adiciona X ou O
+
+		let cloneEl = el.cloneNode(true); // cria uma variavel para clonar dentro dela todo o elemento el
+
+		this.appendChild(cloneEl); // adiciona o clone do elemento dentro da caixa
+
+		// logica para computar cada uma das jogadas de cada um
+		if(jogador1 == jogador2){ // quando o valor dos jogadores estiver igual ele ira somar +1 para o jogador 1
+			jogador1++;
+		}else{ // como a unica outra situação é um valor diferente entre as variaveis ira aumentar o valor do jogador 2 em +1
+			jogador2++;
+		}
+
+		//Checar quem venceu
+
+
+
+	}
+
+
+	});
+
+}
+
+function checarElemento(jogador1, jogador2){
+
+		let el; // variavel criada para receber e repassar a situação de x ou 0
+
+		if(jogador1 == jogador2){ // verifica a situação do valor dentro das variaveis jogadores e se forem iguais realiza a ação
+			// 
+			el = x; // ira colocar dentro da variavel o valor x que representa o elemento x dentro do css
+		} else {
+			el = o; // ira colocar o valor de circulo dentro da variavel que foi criada no html e css
+			//
+		}
+		return el;
+}
+
+function checarVencedor(){
+	let b = document.querySelector(".");
+}
