ca48bd669b2262c311f95b45aebe26f5aeefb7b2
philipheferreira <philipheferreira@gmail.com>
2024-09-09 15:18:58 -0300

    Feat: adjust css and create feacth input infos geral

--- 15-Programacção Assíncrona com JavaScript/2 - projeto API pokemon/script.js
+++ 15-Programacção Assíncrona com JavaScript/2 - projeto API pokemon/script.js
@@ -1,16 +1,37 @@
-//definição de infos do pokemon
+//definição de variaveis que vão receber as infos do pokemon
+let pokemonImagem = document.querySelector(".imagem");
 let pokemonNome = document.querySelector(".nome");
-let imagemPokemon = document.querySelector(".imagem");
+let pokemonId = document.querySelector(".Id");
+let pokemonTipo = document.querySelector(".tipo");
+let pokemonAltura = document.querySelector(".altura");
+let pokemonPeso = document.querySelector(".peso");
 
 const getPokemonUrl = 'https://pokeapi.co/api/v2/' //url da API
 
+//informações que serão repassadas para fazer as buscas na API
+let id = 4
+let numeroTipo = 10;
 
 // chamada da API e depuração da informação
 const getPokemon = async (id) => {
 	try {
 		const responsePokemon = await fetch(getPokemonUrl + "pokemon/" + id); // pega a url e direciona a id pokemon
+		const pokemonTipo = await fetch(getPokemonUrl+ "type/10");
 		const pokemon = await responsePokemon.json(); // converte para info json
+		const tipo = await pokemonTipo.json();
 		return pokemon; // retorna pokemon pesquisado
+
+	} catch(err){
+		console.log(err);
+	}
+}
+
+// chamada para pegar a info de tipo de pokemon
+const getPokemonTipo = async (numeroTipo) => {
+	try {
+		const pokemonTipo = await fetch(getPokemonUrl+ "type/" + numeroTipo);
+		const tipo = await pokemonTipo.json();
+		return tipo; // retorna pokemon pesquisado
 
 	} catch(err){
 		console.log(err);
@@ -19,18 +40,21 @@ const getPokemon = async (id) => {
 
 // definição de id para chamadas e chamadas de todas as funções dentro da chamada assincrona
 (async () => {
-	let id = 4
 	let chamadaPokemonAPI = await getPokemon(id);
-	fecthPokemonInformacoes(chamadaPokemonAPI);
-	imagemPokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/' + id + '.png';
+	let chamadaPokemonTipo = await getPokemonTipo(numeroTipo);
+	fecthPokemonInformacoes(chamadaPokemonAPI, chamadaPokemonTipo);
 	console.log(chamadaPokemonAPI);
 })()
 
-
-let fecthPokemonInformacoes = (chamadaPokemonAPI) => {
+// função que faz os repasses das informações da API para as variaveis no html
+let fecthPokemonInformacoes = (chamadaPokemonAPI, chamadaPokemonTipo) => {
 
 	pokemonNome.innerHTML = chamadaPokemonAPI.name; // repassa nome
-
+	pokemonId.innerHTML = chamadaPokemonAPI.id;
+	pokemonAltura.innerHTML = chamadaPokemonAPI.height;
+	pokemonPeso.innerHTML = chamadaPokemonAPI.weight;
+	pokemonTipo.innerHTML = chamadaPokemonTipo.name;
+	pokemonImagem.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/' + id + '.png';
 }
 
 
