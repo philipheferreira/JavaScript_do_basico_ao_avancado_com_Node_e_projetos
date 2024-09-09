5970b435205d776bd89f4e2cd8fedd3aa4b2467a
philipheferreira <philipheferreira@gmail.com>
2024-09-09 12:51:41 -0300

    feat: create function fecthPokemonInformacoes and add image

--- 15-Programacção Assíncrona com JavaScript/2 - projeto API pokemon/script.js
+++ 15-Programacção Assíncrona com JavaScript/2 - projeto API pokemon/script.js
@@ -1,22 +1,37 @@
-let infoPokemon = document.querySelector(".nome");
+//definição de infos do pokemon
+let pokemonNome = document.querySelector(".nome");
+let imagemPokemon = document.querySelector(".imagem");
 
-const getPokemonUrl = 'https://pokeapi.co/api/v2/'
+const getPokemonUrl = 'https://pokeapi.co/api/v2/' //url da API
 
+
+// chamada da API e depuração da informação
 const getPokemon = async (id) => {
 	try {
-		const responsePokemon = await fetch(getPokemonUrl + "pokemon/" + id);
-		const pokemon = await responsePokemon.json();
-		return pokemon;
+		const responsePokemon = await fetch(getPokemonUrl + "pokemon/" + id); // pega a url e direciona a id pokemon
+		const pokemon = await responsePokemon.json(); // converte para info json
+		return pokemon; // retorna pokemon pesquisado
 
 	} catch(err){
 		console.log(err);
 	}
 }
 
+// definição de id para chamadas e chamadas de todas as funções dentro da chamada assincrona
 (async () => {
-	let pokemon1 = await getPokemon('72');
-	infoPokemon.innerHTML = pokemon1;
-	console.log(pokemon1);
+	let id = 4
+	let chamadaPokemonAPI = await getPokemon(id);
+	fecthPokemonInformacoes(chamadaPokemonAPI);
+	imagemPokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/' + id + '.png';
+	console.log(chamadaPokemonAPI);
 })()
+
+
+let fecthPokemonInformacoes = (chamadaPokemonAPI) => {
+
+	pokemonNome.innerHTML = chamadaPokemonAPI.name; // repassa nome
+
+}
+
 
 
