a11e753307d92c2f8fd3ab761b582227d9da9c72
philipheferreira <philipheferreira@gmail.com>
2024-10-08 14:13:46 -0300

    Feat: range input error

---
+++ 17-Eventos com javascript/projects/8 - range input real time/script.js
@@ -0,0 +1,15 @@
+
+	let passadorX = document.querySelector(".x-deslizar");
+	let passadorY = document.querySelector(".y-deslizar");
+
+	let valorX = document.querySelector(".x");
+	let valorY = document.querySelector(".y");
+	
+
+let atualizar = () => {
+
+	valorX.innerText = passadorX.value;
+	valorY.innerText = passadorY.value;
+}
+
+
