d83f7b9c7aa69b3bea9b5583fc5ea832b1a90a65
philipheferreira <philipheferreira@gmail.com>
2024-11-04 14:50:50 -0300

    Test connection

--- 15-Programacção Assíncrona com JavaScript/3 - projeto API simples/script.js
+++ 15-Programacção Assíncrona com JavaScript/3 - projeto API simples/script.js
@@ -1,1 +1,21 @@
-console.log("Ola, testando");
+	const simpleAPIUrl = 'https://localhost:7122/api/Produtos';
+
+	const getTodosProdutos = async () => {
+		try {
+			const responseTodosProdutos = await fetch(simpleAPIUrl); // pega a url e direciona para o caminho de todos os produtos
+			const Todosprodutos = await responseTodosProdutos.json(); // converte para info json
+			return Todosprodutos;
+
+		} catch(err){
+			console.log(err);
+		}
+	}
+
+	
+
+
+	(async () => {
+		console.log(getTodosProdutos());
+	})()
+
+
