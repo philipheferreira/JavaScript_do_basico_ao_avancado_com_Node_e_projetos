9c4c2fb3a58dd09cd107e625a64c55cc63fd625e
philipheferreira <philipheferreira@gmail.com>
2024-11-04 15:45:18 -0300

    Feat: finish connection simple API project

--- 15-Programacção Assíncrona com JavaScript/3 - projeto API simples/script.js
+++ 15-Programacção Assíncrona com JavaScript/3 - projeto API simples/script.js
@@ -1,6 +1,14 @@
-	let id = document.querySelector(".campoID");
-	let nome = document.querySelector(".campoNome");
-	let preco = document.querySelector(".campoPreco");
+	let id1 = document.querySelector(".campoID1");
+	let nome1 = document.querySelector(".campoNome1");
+	let preco1 = document.querySelector(".campoPreco1");
+
+	let id2 = document.querySelector(".campoID2");
+	let nome2 = document.querySelector(".campoNome2");
+	let preco2 = document.querySelector(".campoPreco2");
+
+	let id3 = document.querySelector(".campoID3");
+	let nome3 = document.querySelector(".campoNome3");
+	let preco3 = document.querySelector(".campoPreco3");
 
 	const simpleAPIUrl = 'https://localhost:7122/api/Produtos';
 
@@ -21,9 +29,17 @@
 	let repasseProdutos = async () => {
 		let chamadaTodosProdutos = await getTodosProdutos();
 
-		id.innerHTML = chamadaTodosProdutos[0].id;
-		nome.innerHTML = chamadaTodosProdutos[0].nome;
-		preco.innerHTML = chamadaTodosProdutos[0].preco;
+		id1.innerHTML = chamadaTodosProdutos[0].id;
+		nome1.innerHTML = chamadaTodosProdutos[0].nome;
+		preco1.innerHTML = chamadaTodosProdutos[0].preco;
+
+		id2.innerHTML = chamadaTodosProdutos[1].id;
+		nome2.innerHTML = chamadaTodosProdutos[1].nome;
+		preco2.innerHTML = chamadaTodosProdutos[1].preco;
+
+		id3.innerHTML = chamadaTodosProdutos[2].id;
+		nome3.innerHTML = chamadaTodosProdutos[2].nome;
+		preco3.innerHTML = chamadaTodosProdutos[2].preco;
 
 		console.log(chamadaTodosProdutos);
 
