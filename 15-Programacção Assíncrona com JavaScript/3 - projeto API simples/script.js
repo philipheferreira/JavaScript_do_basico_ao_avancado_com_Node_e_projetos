cb76dfa0e692187dc1850954c56753f14479547b
philipheferreira <philipheferreira@gmail.com>
2024-11-04 15:34:26 -0300

    Feat: connect sucess html page infos

--- 15-Programacção Assíncrona com JavaScript/3 - projeto API simples/script.js
+++ 15-Programacção Assíncrona com JavaScript/3 - projeto API simples/script.js
@@ -1,5 +1,10 @@
+	let id = document.querySelector(".campoID");
+	let nome = document.querySelector(".campoNome");
+	let preco = document.querySelector(".campoPreco");
+
 	const simpleAPIUrl = 'https://localhost:7122/api/Produtos';
 
+// chamada para o get de todos os produtos
 	const getTodosProdutos = async () => {
 		try {
 			const responseTodosProdutos = await fetch(simpleAPIUrl); // pega a url e direciona para o caminho de todos os produtos
@@ -11,11 +16,25 @@
 		}
 	}
 
-	
+
+// faz o repasse para os campos no front
+	let repasseProdutos = async () => {
+		let chamadaTodosProdutos = await getTodosProdutos();
+
+		id.innerHTML = chamadaTodosProdutos[0].id;
+		nome.innerHTML = chamadaTodosProdutos[0].nome;
+		preco.innerHTML = chamadaTodosProdutos[0].preco;
+
+		console.log(chamadaTodosProdutos);
+
+	}
 
 
+
+// Chamada final para rodar tudo quando abre a aba
 	(async () => {
-		console.log(getTodosProdutos());
+		repasseProdutos();
+
 	})()
 
 
